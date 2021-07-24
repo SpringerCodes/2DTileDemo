6f391b12e3dbb82d320d7b33e2f1cac7ac819809
Chris Springer <topher.springer1@gmail.com>
2021-07-24 10:01:00 -0700

    Add files via upload

---
+++ Scripts/CoinPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int coinValue = 50;
+
+    bool isCollected = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isCollected) { return; }
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore(coinValue);
+        isCollected = true;
+        Destroy(gameObject);
+    }
+}
